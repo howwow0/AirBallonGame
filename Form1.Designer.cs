b06b93a9235554f65b5f5d1423471ba6061e1d0c
HowWow <yarohsyk10@gmail.com>
2024-10-31 16:57:45 +0400

    Create game

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace AirBallonGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(539, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AirBallonGame.Properties.Resources.earth;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 393);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::AirBallonGame.Properties.Resources.cloud;
+            this.pictureBox5.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(258, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::AirBallonGame.Properties.Resources.Baloon;
+            this.pictureBox3.Location = new System.Drawing.Point(91, 315);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::AirBallonGame.Properties.Resources.earth;
+            this.pictureBox2.Location = new System.Drawing.Point(314, 393);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(258, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::AirBallonGame.Properties.Resources.cloud;
+            this.pictureBox6.Location = new System.Drawing.Point(314, 12);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(258, 50);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::AirBallonGame.Properties.Resources.Baloon;
+            this.pictureBox4.Location = new System.Drawing.Point(393, 315);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox6);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Игра";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.PictureBox pictureBox4;
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AirBallonGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool _isGameRunning = false;
+        private readonly Random _random = new Random(DateTime.Now.Millisecond);
+        private const string KeysFirstPlayer = "QWERTASDFZXCV";
+        private const string KeysSecondPlayer = "YUIOPGHJKLBNM";
+        private const string TextWinFirstPlayer = "Выйграл первый игрок.";
+        private const string TextWinSecondPlayer = "Выйграл второй игрок.";
+        private const string TextGameOver = "Игра окончена.";
+        private const string TextStartGame = "Старт игры";
+        private const string TextReadyToGame = "Вы готовы начать игру?";
+        private const int MillisecondsForClick = 1500; // Время на нажатие кнопки
+        private const int StepAirBallons = 15; // Количество пикселей на которое прыгает шар
+        private const int StartHeight = 300; // Стартовая высота шаров
+
+        private string _generatedKeyFirstPlayer = string.Empty;
+        private string _generatedKeySecondPlayer = string.Empty;
+
+        private void GenerateKey(bool isFirstPlayer)
+        {
+            string keys = isFirstPlayer ? KeysFirstPlayer : KeysSecondPlayer;
+            string generatedKey = keys[_random.Next(keys.Length)].ToString();
+            if (isFirstPlayer)
+            {
+                _generatedKeyFirstPlayer = generatedKey;
+                label1.Text = generatedKey;
+            }
+            else
+            {
+                _generatedKeySecondPlayer = generatedKey;
+                label2.Text = generatedKey;
+            }
+        }
+
+        private void MoveBalloon(PictureBox pictureBox, int step)
+        {
+            var point = pictureBox.Location;
+            point.Y += step;
+            pictureBox.Location = point;
+        }
+
+        private void CheckBalloonPosition(PictureBox balloon, bool isFirstPlayer)
+        {
+            bool hasCollided = isFirstPlayer
+                ? IsColliding(balloon, pictureBox1) || IsColliding(balloon, pictureBox5)
+                : IsColliding(balloon, pictureBox2) || IsColliding(balloon, pictureBox6);
+
+            bool winFirstPlayer = isFirstPlayer
+                ? IsColliding(balloon, pictureBox5)
+                : IsColliding(balloon, pictureBox2);
+
+            if (hasCollided)
+            {
+                GameOver(winFirstPlayer);
+            }
+        }
+
+        private void GameOver(bool winFirstPlayer)
+        {
+            _isGameRunning = false;
+            timer1.Stop();
+            timer2.Stop();
+            string gameOverText =
+                $"{TextGameOver} {(winFirstPlayer ? TextWinFirstPlayer : TextWinSecondPlayer)} {TextReadyToGame}";
+            DialogResult result = MessageBox.Show(gameOverText, TextGameOver, MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                StartGame();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void StartGame()
+        {
+            _isGameRunning = true;
+            pictureBox3.Location = new Point(pictureBox3.Location.X, StartHeight);
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox4.Location = new Point(pictureBox4.Location.X, StartHeight);
+            pictureBox4.BackColor = Color.Transparent;
+            GenerateKey(true);
+            GenerateKey(false);
+            ResetTimer(timer1);
+            ResetTimer(timer2);
+        }
+
+        private void ResetTimer(Timer timer)
+        {
+            timer.Interval = MillisecondsForClick;
+            timer.Stop();
+            timer.Start();
+        }
+
+        private bool IsColliding(PictureBox balloon, PictureBox boundary)
+        {
+            return balloon.Bounds.IntersectsWith(boundary.Bounds);
+        }
+
+        private void TimerTick(PictureBox pictureBox, bool isFirstPlayer)
+        {
+            MoveBalloon(pictureBox, StepAirBallons);
+            CheckBalloonPosition(pictureBox, isFirstPlayer);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(TextReadyToGame, TextStartGame, MessageBoxButtons.YesNo, MessageBoxIcon.Question) !=
+                DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                StartGame();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!_isGameRunning) return;
+
+            string keyString = e.KeyCode.ToString().ToUpper();
+            if (keyString == _generatedKeyFirstPlayer)
+            {
+                MoveBalloon(pictureBox3, -StepAirBallons);
+                CheckBalloonPosition(pictureBox3, true);
+                GenerateKey(true);
+                ResetTimer(timer1);
+            }
+            else if (keyString == _generatedKeySecondPlayer)
+            {
+                MoveBalloon(pictureBox4, -StepAirBallons);
+                CheckBalloonPosition(pictureBox4, false);
+                GenerateKey(false);
+                ResetTimer(timer2);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimerTick(pictureBox3, true);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            TimerTick(pictureBox4, false);
+        }
+    }
+}
